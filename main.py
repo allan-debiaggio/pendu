8cb49de3620da013967b37052422733b28b2c4d6
Rayan-tarbi <Rayan.tarbi@laplateforme.io>
2025-01-22 15:23:02 +0100

    :white_check_mark: adding new features Adding automatic local list + selected words if API dont work

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import random
 import json
 import os
 
-# Liste des URLs d'API
 api_urls = [
     "https://trouve-mot.fr/api/categorie/6",
     "https://trouve-mot.fr/api/categorie/19",
@@ -15,26 +14,23 @@ api_urls = [
 
 words = []
 
-# Charger les mots existants depuis le fichier JSON s'il existe
 json_file_path = 'words.json'
 if os.path.exists(json_file_path):
     with open(json_file_path, 'r') as file:
         words = json.load(file)
 
-# Fonction pour choisir un mot aléatoire depuis la liste existante
 def get_random_word():
     if words:
         return random.choice(words)
     return None
 
-# Tenter de récupérer des mots depuis les APIs
 for url in api_urls:
     try:
         response = requests.get(url, timeout=10)
         if response.status_code == 200:
             data = response.json()
             if len(data) > 0:
-                word = data[0].get('name', None)  # Assurez-vous que 'name' est correct
+                word = data[0].get('name', None)
                 if word and word not in words:
                     words.append(word)
             else:
@@ -51,11 +47,9 @@ for url in api_urls:
         print(f"Une erreur inattendue est survenue pour l'API ({url}) : {e}")
         break
 
-# Sauvegarder les mots dans le fichier JSON
 with open(json_file_path, 'w') as file:
     json.dump(words, file, ensure_ascii=False, indent=4)
 
-# Si aucun mot n'a été choisi, en choisir un aléatoirement depuis la liste locale
 selected_word = get_random_word()
 if selected_word:
     print(f"Mot choisi aléatoirement depuis la liste locale : {selected_word}")
