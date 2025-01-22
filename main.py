039e4ee1d5ce2c659ea648f12ce56fd5d0f664a6
Rayan-tarbi <Rayan.tarbi@laplateforme.io>
2025-01-22 15:21:46 +0100

    :white_check_mark: Adding new features Adding automatic local list + selected words if API dont work

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
 import requests
 import random
+import json
+import os
 
-api_urls = [ #API URLs
+# Liste des URLs d'API
+api_urls = [
     "https://trouve-mot.fr/api/categorie/6",
     "https://trouve-mot.fr/api/categorie/19",
     "https://trouve-mot.fr/api/categorie/16",
@@ -12,26 +15,51 @@ api_urls = [ #API URLs
 
 words = []
 
+# Charger les mots existants depuis le fichier JSON s'il existe
+json_file_path = 'words.json'
+if os.path.exists(json_file_path):
+    with open(json_file_path, 'r') as file:
+        words = json.load(file)
+
+# Fonction pour choisir un mot aléatoire depuis la liste existante
+def get_random_word():
+    if words:
+        return random.choice(words)
+    return None
+
+# Tenter de récupérer des mots depuis les APIs
 for url in api_urls:
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        if len(data) > 0:
-            word = data[0].get('name', None)  
-            if word:
-                words.append(word)
+    try:
+        response = requests.get(url, timeout=10)
+        if response.status_code == 200:
+            data = response.json()
+            if len(data) > 0:
+                word = data[0].get('name', None)  # Assurez-vous que 'name' est correct
+                if word and word not in words:
+                    words.append(word)
+            else:
+                print(f"Aucune donnée reçue depuis l'API : {url}")
         else:
-            print(f"Aucune donnée reçue depuis l'API : {url}")
-    else:
-        print(f"Erreur lors de la requête API ({url}) : {response.status_code}")
+            print(f"Erreur lors de la requête API ({url}) : {response.status_code}")
+    except requests.exceptions.Timeout:
+        print(f"Requête API ({url}) a expiré après 10 secondes.")
+        break
+    except requests.exceptions.ConnectionError as e:
+        print(f"Erreur de connexion à l'API. Chargement de la liste locale")
+        break
+    except Exception as e:
+        print(f"Une erreur inattendue est survenue pour l'API ({url}) : {e}")
+        break
 
-if words:
-    selected_word = random.choice(words)
-    print(f"Mot choisi aléatoirement : {selected_word}")
+# Sauvegarder les mots dans le fichier JSON
+with open(json_file_path, 'w') as file:
+    json.dump(words, file, ensure_ascii=False, indent=4)
+
+# Si aucun mot n'a été choisi, en choisir un aléatoirement depuis la liste locale
+selected_word = get_random_word()
+if selected_word:
+    print(f"Mot choisi aléatoirement depuis la liste locale : {selected_word}")
 else:
-    selected_word = None
-    print("Aucun mot n'a été récupéré des APIs.")
-
-
+    print("Aucun mot n'est disponible dans la liste locale.")
 
 pygame.init()
